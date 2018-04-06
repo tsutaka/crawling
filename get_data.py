ca28d4e34f3f3fe0c0d9cfd16f2ff206c3251767
tsutaka <tomasen3@gmail.com>
2018-04-07 00:34:17 +0900

    [update]取得対象変更

--- get_data.py
+++ get_data.py
@@ -2,13 +2,19 @@ from bs4 import BeautifulSoup
 import urllib.request as req
 
 # HTMLを取得
-url = "http://stocks.finance.yahoo.co.jp/stocks/detail/?code=usdjpy"
+url = "https://baseball.yahoo.co.jp/npb/standings/"
 res = req.urlopen(url)
 
 # HTMLを解析
 soup = BeautifulSoup(res, "html.parser")
 
-# 任意のデータを抽出 --- (※1)
-price = soup.select_one(".stoksPrice").string
-print("usd/jpy=", price)
 
+
+# #sta_c > div:nth-child(1) > table:nth-child(2) > tbody:nth-child(1) > tr:nth-child(2) > td:nth-child(2) > a:nth-child(1)
+li_list = soup.select("div > table > tbody > tr > td")
+for li in li_list:
+    a = li.a
+    if a != None:
+        name = a.string
+        href = a.attrs["href"]
+        print(name, ">", href)
